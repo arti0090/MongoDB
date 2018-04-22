c199eea3d3e43b65ab8f4bb66a49aa8ec097fafc
arti0090 <arti0090@gmail.com>
2018-04-22 15:27:00 +0200

    dziala wow

--- findNested.js
+++ findNested.js
@@ -1,16 +1,22 @@
-db = connect("localhost/films")
+db = connect("localhost:27017/films")
 
-use films
+// use films
 
 db.film.find({"year":{$gt:2000}},{"title":1,_id:0,"year":1}).sort({"title":1})
 
-db.people.aggregate(
+cursor = db.film.aggregate(
   [
     {
       $group:{
-        _id:'gender',
+        _id:'$year',
         total: {$sum:1}
       }
     }
   ]
 )
+
+while ( cursor.hasNext() ) {
+   printjson( cursor.next() );
+}
+
+
